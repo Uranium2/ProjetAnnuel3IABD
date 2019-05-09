7b141059763d4cabce40ffe391582bf05cd2c3c9
Tavernier Antoine DESKTOP <tavernier.antoine@hotmail.fr>
2019-05-09 18:11:45 +0200

    [Web] Simple Upload image front/back in Flask

---
+++ web/src/app.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask, request, render_template, send_from_directory
+
+__author__ = 'Group6 3IABD'
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        dest = "/".join([target, "tempImg.PNG"])
+        print(dest)
+        file.save(dest)
+    
+    return render_template("complete.html")
+
+if __name__ == "__main__":
+    app.run(port=4555, debug=True)
