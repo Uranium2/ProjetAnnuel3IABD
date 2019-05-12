02f97858bbf143c341388baf9259f0212440304d
Tavernier Antoine DESKTOP <tavernier.antoine@hotmail.fr>
2019-05-12 21:05:06 +0200

    [UNITY] Shoot + Stop on grid! Need to select a color/Class + send to website

---
+++ VisualNN/Assets/StopOnPlane.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopOnPlane : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody>())
+        {
+            Vector3 pos = col.gameObject.transform.position;
+            pos.z = 0;
+            col.gameObject.transform.position = pos;
+            Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            Debug.Log("Character Hit");
+        }
+    }
+}
