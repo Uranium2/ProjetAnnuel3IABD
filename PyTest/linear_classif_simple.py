7dbcf97e0f8df2c492279d790fa87c7bc893e4d7
Tavernier Antoine DESKTOP <tavernier.antoine@hotmail.fr>
2019-06-26 15:12:13 +0200

    [Python] use case

--- PyTest/linear_classif_simple.py
+++ PyTest/linear_classif_simple.py
@@ -4,34 +4,41 @@ import numpy as np
 import random
 
 if __name__ == "__main__":
-    sampleCount = 4
+    sampleCount = 8
     inputCountPerSample = 2
     alpha = 0.02
-    epochs = 2000
+    epochs = 200
     YTrain = []
-    XTrain = []
+    XTrain = [
+        0, 0,
+        0, 0.5,
+        0.5, 0,
+        0.5, 0.5,
+        1, 1,
+        1, 1.5,
+        1.5, 1,
+        1.5, 1.5
+    ]
+    YTrainKmeans = []
     K = 2
 
-    for x in np.arange(0, sampleCount / 2):
-        XTrain.append(random.uniform(0, 1) * 0.9)
-        XTrain.append(random.uniform(0, 1) * 0.9)
 
-    for x in np.arange(0, sampleCount / 2):
-        XTrain.append(random.uniform(1, 2) * 0.9)
-        XTrain.append(random.uniform(1, 2) * 0.9)
+    for val in np.arange(0, sampleCount / 2):
+        YTrain.append(-1)
+    for val in np.arange(sampleCount / 2, sampleCount):
+        YTrain.append(1)
 
-    #for val in np.arange(0, sampleCount / 2):
-    #    YTrain.append(-1)
-    #for val in np.arange(sampleCount / 2, sampleCount):
-    #    YTrain.append(1)
-    YTrain.append(1)
-    YTrain.append(-1)
+    YTrainKmeans.append(1)
+    YTrainKmeans.append(-1)
     W = create_linear_model(inputCountPerSample)
-    print("create_linear_model")
-    XTrainKmeans = get_Kmeans(K,  XTrain, sampleCount, inputCountPerSample, 2)
-    print("get_Kmeans")
-    fit_classification_rosenblatt_rule(
-        W, XTrainKmeans, K, inputCountPerSample, YTrain, alpha, epochs
-    )
+    WKmeans = create_linear_model(inputCountPerSample)
 
-    predict_2D(W, inputCountPerSample, XTrainKmeans, YTrain, 0, 2)
+    XTrainKmeans = get_Kmeans(K,  XTrain, sampleCount, inputCountPerSample, 2)
+
+    fit_classification_rosenblatt_rule(W, XTrain, sampleCount, inputCountPerSample, YTrain, alpha, epochs)
+
+    fit_classification_rosenblatt_rule(WKmeans, XTrainKmeans, K, inputCountPerSample, YTrainKmeans, alpha, epochs)
+
+    predict_2D(W, inputCountPerSample, XTrain, YTrain, 0, 2)
+    predict_2D(WKmeans, inputCountPerSample, XTrainKmeans, YTrainKmeans, 0, 2)
+
