c531e2829fa62f030a2e34a8862f04abc7f5496c
Tavernier Antoine <tavernier.antoine@hotmail.fr>
2019-07-01 18:35:10 +0200

    [Linear] Test file

--- PyTest/linear_dataset.py
+++ PyTest/linear_dataset.py
@@ -1,52 +1,103 @@
 from dll_load import create_linear_model, fit_classification_rosenblatt_rule, saveLinearModel, loadLinearModel, predict_regression
-from load_img import getDataSet
+from load_img import getDataSet, getDataSetTest
 from PIL import Image
 
 
 if __name__ == "__main__":
     def fit_save():
-        img_per_folder = 1
-        h = 1000
-        w = 1000
+        img_per_folder = 1200
+        h = 100
+        w = 100
         inputCountPerSample = h * w * 3
         sampleCount = img_per_folder * 3
         
-        alpha = 0.04
+        alpha = 0.05
         epochs = 500
 
         XTrain, Y = getDataSet("../img", img_per_folder, h, w)
 
-        W = create_linear_model(inputCountPerSample)
-        YTrain = []
+        W_FPS = create_linear_model(inputCountPerSample)
+        W_MOBA = create_linear_model(inputCountPerSample)
+        W_RTS = create_linear_model(inputCountPerSample)
+        YTrain_FPS = []
+        YTrain_MOBA = []
+        YTrain_RTS = []
 
         for i in range(img_per_folder * 3):
             if i < img_per_folder:
-                YTrain.append(1)
+                YTrain_FPS.append(1)
             else:
-                YTrain.append(-1)
+                YTrain_FPS.append(-1)
+        for i in range(img_per_folder * 3):
+            if i > img_per_folder and i < 2 * img_per_folder:
+                YTrain_MOBA.append(1)
+            else:
+                YTrain_MOBA.append(-1)
+        for i in range(img_per_folder * 3):
+            if i > 2 * img_per_folder and i < 3 * img_per_folder:
+                YTrain_RTS.append(1)
+            else:
+                YTrain_RTS.append(-1)
 
-        fit_classification_rosenblatt_rule(W, XTrain, sampleCount, inputCountPerSample, YTrain, alpha, epochs)
-        file_name = "linear_dataset_" + str(epochs) + ".model"
+        fit_classification_rosenblatt_rule(W_FPS, XTrain, sampleCount, inputCountPerSample, YTrain_FPS, alpha, epochs)
+        fit_classification_rosenblatt_rule(W_MOBA, XTrain, sampleCount, inputCountPerSample, YTrain_MOBA, alpha, epochs)
+        fit_classification_rosenblatt_rule(W_RTS, XTrain, sampleCount, inputCountPerSample, YTrain_RTS, alpha, epochs)
+        file_name_FPS = "linear_dataset_FPS_rendu3_18_30_" + str(img_per_folder) + ".model"
+        file_name_MOBA = "linear_dataset_MOBA_rendu3_18_30_" + str(img_per_folder) + ".model"
+        file_name_RTS = "linear_dataset_RTS_rendu3_18_30_" + str(img_per_folder) + ".model"
 
-        saveLinearModel(W, inputCountPerSample, file_name)
+        saveLinearModel(W_FPS, inputCountPerSample, file_name_FPS)
+        saveLinearModel(W_MOBA, inputCountPerSample, file_name_MOBA)
+        saveLinearModel(W_RTS, inputCountPerSample, file_name_RTS)
 
     def load_predict():
-        h = 1000
-        w = 1000
-        inputCountPerSample, W = loadLinearModel("mlp_dataset_16_16.model")
-        XTest = []
-        im = Image.open('../img/RTS/RTS_0000.png') 
-        imResize = im.resize((h, w), Image.ANTIALIAS)
-        imgLoad = imResize.load()
-        for x in range(h):
-            for y in range(w):
-                R,G,B = imgLoad[x, y]
-                XTest.append(R)
-                XTest.append(G)
-                XTest.append(B)
-        im.close()
-        res = predict_regression(W, XTest, inputCountPerSample)
-        print(res)
-    
+        img_per_folder = 60
+        h = 25
+        w = 25
+
+        Xpredict = []
+
+        Ypredict_FPS = []
+        Ypredict_MOBA = []
+        Ypredict_RTS = []
+
+        inputCountPerSample, WFPS = loadLinearModel("linear_dataset_FPS_rendu3_18_30_1200.model")
+        inputCountPerSample, WMOBA = loadLinearModel("linear_dataset_MOBA_rendu3_18_30_1200.model")
+        inputCountPerSample, WRTS = loadLinearModel("linear_dataset_RTS_rendu3_18_30_1200.model")
+
+        XTest, Y = getDataSetTest("../img", img_per_folder, h, w)
+        for img in range(img_per_folder * 3):
+            for i in range(h * w * 3):
+                Xpredict.append(XTest[img * i])
+            Ypredict_FPS.append(predict_regression(WFPS, Xpredict, inputCountPerSample))
+            Ypredict_MOBA.append(predict_regression(WMOBA, Xpredict, inputCountPerSample))
+            Ypredict_RTS.append(predict_regression(WRTS, Xpredict, inputCountPerSample))
+            Xpredict.clear()
+
+        #Predict FPS
+        result = []
+        for y in range(len(Ypredict_FPS)):
+            if Ypredict_FPS[y] > Ypredict_MOBA[y] and Ypredict_FPS[y] > Ypredict_RTS[y]:
+                result.append(0)
+            if Ypredict_MOBA[y] > Ypredict_FPS[y] and Ypredict_MOBA[y] > Ypredict_RTS[y]:
+                result.append(1)
+            if Ypredict_RTS[y] > Ypredict_MOBA[y] and Ypredict_RTS[y] > Ypredict_FPS[y]:
+                result.append(2)
+
+
+        stat = []
+        for i in range(img_per_folder):
+            if result[i] == 0 and i < img_per_folder:
+                stat.append(True)
+            elif result[i + img_per_folder] == 1 and i > img_per_folder and i < 2 * img_per_folder:
+                stat.append(True)
+            elif result[i + (2 * img_per_folder)] == 2 and i > 2 * img_per_folder and i < 3 * img_per_folder:
+                stat.append(True)
+            else :
+                stat.append(False)
+        print( sum(stat)/ len(stat) * 100)
+        
+
+
     fit_save()
     load_predict()
--- PyTest/load_img.py
+++ PyTest/load_img.py
@@ -15,6 +15,45 @@ def getDataSet(path="..\img", limit=10, h=10, w=10):
     # r=root, d=directories, f = files
     for r, directories, f in os.walk(path):
         directories[:] = [d for d in directories if d not in exclude]
+        for file in f:
+            if i % (limit + 1) == 0:
+                i = 1
+                yindex  = yindex + 1
+                break
+            if '.png' in file:
+                files.append(os.path.join(r, file))
+                i = i + 1
+                for y in range(3):
+                    YTrain.append(output[yindex][y])
+
+    XTrain = []
+    for f in files:
+        im = Image.open(f)
+        imResize = im.resize((h, w), Image.ANTIALIAS)
+        imgLoad = imResize.load()
+        for x in range(h):
+            for y in range(w):
+                R,G,B = imgLoad[x, y]
+                XTrain.append(R)
+                XTrain.append(G)
+                XTrain.append(B)
+        im.close()
+
+    return XTrain, YTrain
+
+def getDataSetTest(path="..\img", limit=10, h=10, w=10):
+    i = 1
+    files = []
+    exclude = ['FPS', 'MOBA', 'RTS']
+    output = [[1,-1,-1],
+                [-1,1,-1],
+                [-1,-1,1]]
+    YTrain = []
+    yindex = 0
+
+    # r=root, d=directories, f = files
+    for r, directories, f in os.walk(path):
+        directories[:] = [d for d in directories if d not in exclude]
 
         for file in f:
             if i % (limit + 1) == 0:
